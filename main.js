20963f1aa87cce8daef70112bbca24e09ef5ad84
sejal shrivastava <sejalshrivastava32@gmail.com>
2024-06-25 17:16:16 +0530

    javascript
    datatype

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+let firstNumber = 42;
+console.log(firstNumber);
+
+let floatNumber =10.20;
+console.log(floatNumber);
+
+let singleQuotestr = 'Hello , World';
+console.log(singleQuotestr);
+
+let doubleQuoter ="Hello, World";
+console.log(doubleQuoter);
